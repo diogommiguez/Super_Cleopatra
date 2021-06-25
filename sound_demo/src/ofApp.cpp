16778918d56c2d21a39f0c0a45b51e0b59e87f71
Diogo Miguez <diogomiguez@MacBook-Pro-2.local>
2021-06-25 21:49:03 +0100

    coexistencia

--- sound_demo/src/ofApp.cpp
+++ sound_demo/src/ofApp.cpp
@@ -1,13 +1,103 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
+void Ball::set_posx(double x){
+    posx = x;
+}
+
+//--------------------------------------------------------------
+void Ball::move_x(double dx){
+    posx += dx;
+}
+
+//--------------------------------------------------------------
+void Ball::set_posy(double y){
+    posy = y;
+}
+
+//--------------------------------------------------------------
+void Ball::move_y(double dy){
+    posy += dy;
+}
+
+//--------------------------------------------------------------
+void Ball::set_radius(int r){
+    radius = r;
+}
+
+//--------------------------------------------------------------
+void Ball::set_color(int hue){
+    color.setHsb(hue, 255, 255);
+}
+
+//--------------------------------------------------------------
+const double Ball::get_posx(){
+    return posx;
+}
+
+//--------------------------------------------------------------
+const double Ball::get_posy(){
+    return posy;
+}
+
+//--------------------------------------------------------------
+const int Ball::get_radius(){
+    return radius;
+}
+
+//--------------------------------------------------------------
+const double Ball::get_v_y(){
+    return v_y;
+}
+
+//--------------------------------------------------------------
+void Ball::set_v_y(double _v_y){
+    v_y = _v_y;
+}
+
+//--------------------------------------------------------------
+void Ball::set_v_x(double _v_x){
+    v_x = _v_x;
+}
+
+//--------------------------------------------------------------
+const ofColor Ball::get_color(){
+    return color;
+}
+
+//--------------------------------------------------------------
+void Ball::evolve(){
+    double aux_x = posx;
+    double aux_y = posy;
+
+    posx += dt*v_x + dt*dt*a*(ofGetMouseX()-posx)/(0.01+abs(pow(ofGetMouseX()-posx,1)));
+    posy += dt*v_y + dt*dt*(g+(ofGetMouseY()-posy)*a/(0.01+abs(pow(ofGetMouseY()-posy,1))));
+
+    v_x= (posx-aux_x)/dt;
+    v_y= (posy-aux_y)/dt;
+    
+    if(posy >ofGetHeight()-radius && closed_floor ){
+            posy = ofGetHeight()-radius;
+            set_v_y(-get_v_y());
+            getNote(get_posx());
+        }
+
+}
+
+//--------------------------------------------------------------
+void Ball::getNote(double xpos){
+    int note = xpos*5/ofGetWidth()+1;
+    cout << "\n" << note << endl;
+}
+
+//--------------------------------------------------------------
 void ofApp::setup(){
-    nballs = 3;
+    nballs = 2;
     for(int j =0;j<nballs;j++){
         phase.push_back(0);
     }
-    attack = 100;
-    decay = 30000;
+    attack = 500;
+    decay = 3000;
     
     ofSoundStreamSetup(2, 0); // 2 output channels (stereo), 0 input channels
 }
@@ -19,10 +109,10 @@ void ofApp::audioOut( ofSoundBuffer &outBuffer) {
         sample=0;
         for(int ball=0; ball < nballs ;ball++)
         {
-            sample += sin(0.5*phase[ball])*envelope(phase[ball]); // generating a sine wave sample
+            sample += sin(0.5*(ball+1)*phase[ball])*envelope(phase[ball]); // generating a sine wave sample
             phase[ball] += 0.05;
         }
-            //float sample = sin(phase)*envelope(phase) + sin(3.3*phase1)*envelope(phase1); // generating a sine wave sample
+        //float sample = sin(phase[0]);//*envelope(phase[0]); // generating a sine wave sample
             outBuffer[i] = sample; // writing to the left channel
             outBuffer[i + 1] = sample; // writing to the right channel
     }
@@ -34,16 +124,38 @@ double ofApp::envelope(double phase){
         return phase/attack;
     else if(phase < attack+decay)
         return (attack-phase)/decay + 1;
-
+    else if(phase >= attack+decay)
+        return 0;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    double time = ofGetSystemTimeMillis()/1000.0;
+    for(int i = 0; i<balls.size(); i++){
+        balls[i].evolve();
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    for(int i = 0; i<balls.size(); i++){
+        ofSetColor(balls[i].get_color());
+        ofDrawCircle(balls[i].get_posx(), balls[i].get_posy(), balls[i].get_radius());
+    }
+}
 
+//--------------------------------------------------------------
+void ofApp::move(){
+    for(int i = 0; i<balls.size(); i++){
+        balls[i].set_v_x(2000);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::open_floor(){
+    for(int i = 0; i<balls.size(); i++){
+        balls[i].set_closed_floor(false);
+    }
 }
 
 //--------------------------------------------------------------
@@ -52,16 +164,36 @@ void ofApp::keyPressed(int key){
         case 'f':
             ofToggleFullscreen();
             break;
+        case 'q':
+            OF_EXIT_APP(0);
+            break;
+        case ' ':
+            move();
+            break;
+        case 'o':
+            open_floor();
+            break;
         case 'a':
             phase[0]=0;
             break;
         case 'b':
             phase[1]=0;
             break;
-        case 'c':
-            phase[2]=0;
-            break;
+//        case 'c':
+//            phase[2]=0;
+//            break;
     }
+}
+
+//--------------------------------------------------------------
+void ofApp::newBall(int xball, int yball){
+    Ball myball;
+    myball.set_posx(xball);
+    myball.set_posy(yball);
+    myball.set_radius(ofRandom(20,30));
+    myball.set_color(ofRandom(0,255));
+    
+    balls.push_back(myball);
 }
 
 //--------------------------------------------------------------
@@ -81,7 +213,7 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    newBall(ofGetMouseX(),ofGetMouseY());
 }
 
 //--------------------------------------------------------------
--- sound_demo/src/ofApp.h
+++ sound_demo/src/ofApp.h
@@ -7,6 +7,37 @@
 
 #include "ofMain.h"
 
+class Ball : public ofBaseApp{
+        double posx;
+        double posy;
+        int radius;
+        ofColor color;
+        double g = 5000;
+        double a = 1000;
+        double v_y = 0;
+         double v_x = 0;
+        int SAMPLE_RATE = 100;
+        double dt = 1.0/SAMPLE_RATE;
+        bool closed_floor = true;
+    public:
+        void set_posx(double);
+        void move_x(double);
+        void set_posy(double);
+        void move_y(double);
+        void set_radius(int);
+        void set_color(int);
+        void set_v_y(double);
+        void set_v_x(double);
+        const double get_posx();
+        const double get_posy();
+        const int get_radius();
+        const ofColor get_color();
+        const double get_v_y();
+        void evolve();
+        void set_closed_floor(bool val) {closed_floor = val;};
+        void getNote(double);
+};
+
 class ofApp : public ofBaseApp{
 
 	public:
@@ -25,6 +56,12 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    //balls
+        void newBall(int, int);
+        void move();
+        vector <Ball> balls;
+        void open_floor();
+    //audio
         void audioOut(ofSoundBuffer& );
         double envelope(double);
         vector<double> phase;
