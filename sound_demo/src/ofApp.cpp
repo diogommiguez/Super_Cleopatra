1d317dcbd0968cbcb3ea630580b96e8162177ebf
Diogo Miguez <diogomiguez@MacBook-Pro-2.Home>
2021-06-26 17:46:09 +0100

    sliders not working

--- sound_demo/src/ofApp.cpp
+++ sound_demo/src/ofApp.cpp
@@ -6,6 +6,84 @@ double distance(double x1, double y1, double x2, double y2)
     return pow(pow(x1 - x2, 2) + pow(y1 - y2, 2),1/2.0);
 }
 
+//--------------------------------------------------------------//
+// TOGGLE                                                       //
+//--------------------------------------------------------------//
+void toggle::toggle_status(){
+    if(status)
+        status = false;
+    else
+        status = true;
+}
+void toggle::setup(int x, int y)
+{
+    posx = x;
+    posy = y;
+}
+
+void toggle::run(){
+    ofNoFill();
+    ofDrawRectangle(posx, posy, size, size);
+    
+    ofFill();
+    if (not ofGetMousePressed())
+        mouse_previous_pressed = false;
+    if (ofGetMousePressed() and (not mouse_previous_pressed)){
+        mouse_previous_pressed = true;
+        if (((abs(ofGetMouseX() - posx - size/2.0) < size/2.0) and abs(ofGetMouseY() - posy - size/2.0) < size/2.0))
+        {
+            cout << "toggle x: " << posx << "\nrato x: " << ofGetMouseX() << endl;
+            cout << "toggle y: " << posy << "\nrato y: " << ofGetMouseY() << endl;
+            ofDrawRectangle(posx + 3, posy + 3, size - 6, size - 6);
+            toggle_status();
+        }
+    }
+    if (status == true){
+        ofDrawRectangle(posx + 3, posy + 3, size - 6, size - 6);
+    }
+}
+
+//--------------------------------------------------------------//
+// SLIDER                                                       //
+//--------------------------------------------------------------//
+int slider::get_posx(){
+    return posx;
+}
+
+int slider::get_posy(){
+    return posy;
+}
+
+int slider::get_value(){
+    return value;
+}
+
+void slider::setup(int x, int y){
+    posx = x;
+    posy = y;
+    
+    ticker_y = posy + height - (height/100.0 + 5);
+}
+
+void slider::run(){
+    ofNoFill();
+    ofDrawRectangle(posx, posy, width, height);
+    int altura_ticker = height/100 + 5;
+    ofDrawRectangle(posx, ticker_y, width, altura_ticker); //Ticker
+    
+    if (ofGetMousePressed() and ((abs(ofGetMouseX() - posx - width/2.0) < width/2.0 - altura_ticker/2.0) and abs(ofGetMouseY() - posy - height/2.0) < height/2.0 - altura_ticker/2.0))
+    {
+        //ofFill();
+        ticker_y = (int)(ofGetMouseY() - altura_ticker/2.0 + 1);
+    }
+    value = 102*(100 - 100*(ticker_y - (posy + altura_ticker))/height  - 12)/93;
+    ofFill();
+}
+
+//--------------------------------------------------------------//
+// BALL                                                         //
+//--------------------------------------------------------------//
+
 //--------------------------------------------------------------
 void Ball::set_posx(double x){
     posx = x;
@@ -76,8 +154,15 @@ void Ball::evolve(){
     //PEGAR NA BOLA
     if (ofGetMousePressed() and (distance(posx, posy, ofGetMouseX(), ofGetMouseY()) < radius or caught))
     {
+        double aux_x = posx;
+        double aux_y = posy;
+        
         posx = ofGetMouseX();
         posy = ofGetMouseY();
+        
+        v_x= (posx-aux_x)/dt;
+        v_y= (posy-aux_y)/dt;
+        
         caught = true;
     } else {
       
@@ -87,8 +172,10 @@ void Ball::evolve(){
     double aux_x = posx;
     double aux_y = posy;
 
-    posx += dt*v_x + dt*dt*a*(ofGetMouseX()-posx)/(0.01+abs(pow(ofGetMouseX()-posx,1)));
-    posy += dt*v_y + dt*dt*(g+(ofGetMouseY()-posy)*a/(0.01+abs(pow(ofGetMouseY()-posy,1))));
+    int gravity = (mouse_g==true)? 1 : 0 ;
+        
+    posx += dt*v_x + gravity*dt*dt*a*(ofGetMouseX()-posx)/(0.01+abs(pow(ofGetMouseX()-posx,1)));
+    posy += dt*v_y + dt*dt*(g+gravity*(ofGetMouseY()-posy)*a/(0.01+abs(pow(ofGetMouseY()-posy,1))));
 
     v_x= (posx-aux_x)/dt;
     v_y= (posy-aux_y)/dt;
@@ -115,13 +202,37 @@ int Ball::getNote(){
     return note;
 }
 
+//--------------------------------------------------------------//
+// ofApp                                                        //
+//--------------------------------------------------------------//
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-    attack = 300;
-    decay = 1500;
+    attack = 100;
+    decay = 100;
     
     ofSoundStreamSetup(2, 0); // 2 output channels (stereo), 0 input channels
     ofSetCircleResolution(100);
+    
+    mech_menu.menu_x = synth_menu.menu_x;
+    mech_menu.menu_y = synth_menu.menu_y + synth_menu.menu_height + 20;
+    
+    synth_menu.button1.setup(synth_menu.menu_x+synth_menu.menu_width/8.0,synth_menu.menu_y+synth_menu.menu_height/2.0);
+    synth_menu.button2.setup(synth_menu.menu_x+2*synth_menu.menu_width/8.0,synth_menu.menu_y+synth_menu.menu_height/2.0);
+    synth_menu.button3.setup(synth_menu.menu_x+3*synth_menu.menu_width/8.0,synth_menu.menu_y+synth_menu.menu_height/2.0);
+   
+    synth_menu.slider1.setup(synth_menu.menu_x+4*synth_menu.menu_width/8.0,synth_menu.menu_y+25);
+    synth_menu.slider2.setup(synth_menu.menu_x+5*synth_menu.menu_width/8.0,synth_menu.menu_y+25);
+    synth_menu.slider3.setup(synth_menu.menu_x+6*synth_menu.menu_width/8.0,synth_menu.menu_y+25);
+    synth_menu.slider4.setup(synth_menu.menu_x+7*synth_menu.menu_width/8.0,synth_menu.menu_y+25);
+
+    mech_menu.button1.setup(mech_menu.menu_x+mech_menu.menu_width/7.0,mech_menu.menu_y+mech_menu.menu_height/2.0);
+    mech_menu.button2.setup(mech_menu.menu_x+2*mech_menu.menu_width/7.0,mech_menu.menu_y+mech_menu.menu_height/2.0);
+    mech_menu.button3.setup(mech_menu.menu_x+3*mech_menu.menu_width/7.0,mech_menu.menu_y+mech_menu.menu_height/2.0);
+   
+    mech_menu.slider1.setup(mech_menu.menu_x+4*mech_menu.menu_width/7.0,mech_menu.menu_y+25);
+    mech_menu.slider2.setup(mech_menu.menu_x+5*mech_menu.menu_width/7.0,mech_menu.menu_y+25);
+    mech_menu.slider3.setup(mech_menu.menu_x+6*mech_menu.menu_width/7.0,mech_menu.menu_y+25);
     
 }
 
@@ -132,7 +243,7 @@ void ofApp::audioOut( ofSoundBuffer &outBuffer) {
         sample=0;
         for(int ball=0; ball < N ;ball++)
         {
-            sample += sin(balls[ball].getNote()*0.5*(1+ball/2.0)*phase[ball])*envelope(phase[ball]); // generating a sine wave sample
+            sample += sin(balls[ball].getNote()*phase[ball])*envelope(phase[ball]); // generating a sine wave sample
             phase[ball] += 0.05;
         }
         //float sample = sin(phase[0]);//*envelope(phase[0]); // generating a sine wave sample
@@ -149,25 +260,42 @@ double ofApp::envelope(double phase){
         return (attack-phase)/decay + 1;
     else if(phase >= attack+decay)
         return 0;
+    return 0;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    double time = ofGetSystemTimeMillis()/1000.0;
-    for(int i = 0; i<balls.size(); i++){
+
+    for(int i = 0; i<balls.size() and !pause; i++){
         if(balls[i].hit_note==true){
             phase[i] = 0;
         }
         balls[i].evolve();
     }
+    synth_menu.button1.setup(synth_menu.menu_x+synth_menu.menu_width/8.0,synth_menu.menu_y+synth_menu.menu_height/2.0);
+    synth_menu.button2.setup(synth_menu.menu_x+2*synth_menu.menu_width/8.0,synth_menu.menu_y+synth_menu.menu_height/2.0);
+    synth_menu.button3.setup(synth_menu.menu_x+3*synth_menu.menu_width/8.0,synth_menu.menu_y+synth_menu.menu_height/2.0);
+   
+    synth_menu.slider1.setup(synth_menu.menu_x+4*synth_menu.menu_width/8.0,synth_menu.menu_y+25);
+    synth_menu.slider2.setup(synth_menu.menu_x+5*synth_menu.menu_width/8.0,synth_menu.menu_y+25);
+    synth_menu.slider3.setup(synth_menu.menu_x+6*synth_menu.menu_width/8.0,synth_menu.menu_y+25);
+    synth_menu.slider4.setup(synth_menu.menu_x+7*synth_menu.menu_width/8.0,synth_menu.menu_y+25);
+
+    mech_menu.button1.setup(mech_menu.menu_x+mech_menu.menu_width/7.0,mech_menu.menu_y+mech_menu.menu_height/2.0);
+    mech_menu.button2.setup(mech_menu.menu_x+2*mech_menu.menu_width/7.0,mech_menu.menu_y+mech_menu.menu_height/2.0);
+    mech_menu.button3.setup(mech_menu.menu_x+3*mech_menu.menu_width/7.0,mech_menu.menu_y+mech_menu.menu_height/2.0);
+   
+    mech_menu.slider1.setup(mech_menu.menu_x+4*mech_menu.menu_width/7.0,mech_menu.menu_y+25);
+    mech_menu.slider2.setup(mech_menu.menu_x+5*mech_menu.menu_width/7.0,mech_menu.menu_y+25);
+    mech_menu.slider3.setup(mech_menu.menu_x+6*mech_menu.menu_width/7.0,mech_menu.menu_y+25);
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofColor colorOne(0, 50, 255);
-   ofColor colorTwo(50, 0, 100);
+    ofColor colorOne(50, 25,200);
+    ofColor colorTwo(150, 20, 150);
 
-   ofBackgroundGradient(colorOne, colorTwo, OF_GRADIENT_CIRCULAR);
+    ofBackgroundGradient(colorOne, colorTwo, OF_GRADIENT_LINEAR);
 
     for(int i = 0; i<balls.size(); i++){
         ofSetColor(balls[i].get_color());
@@ -177,12 +305,53 @@ void ofApp::draw(){
         ofSetColor(i*255/5, 100*(1+sin(i)), 255-i*255/5);
         ofDrawRectangle(i*ofGetWidth()/5, ofGetHeight()-15, (i+1)*ofGetWidth()/5, ofGetHeight());
     }
+    if(synth_menu.pop_menu){
+        ofSetColor(100,100,100);
+        ofDrawRectangle(synth_menu.menu_x-5, synth_menu.menu_y-5, synth_menu.menu_width+10, synth_menu.menu_height+10);
+        ofSetColor(150,150,150);
+        ofDrawRectangle(synth_menu.menu_x, synth_menu.menu_y, synth_menu.menu_width, synth_menu.menu_height);
+        ofSetColor(150,0,255);
+        ofDrawRectangle(synth_menu.menu_x, synth_menu.menu_y, synth_menu.menu_width, 5);
+        
+        ofSetColor(0,0,255);
+        // in draw:
+        ofDrawBitmapString("SYNTH MENU", synth_menu.menu_x+synth_menu.menu_width/2.0-105,synth_menu.menu_y+synth_menu.menu_height/6.0);
+        
+        synth_menu.button1.run();
+        synth_menu.button2.run();
+        synth_menu.button3.run();
+        synth_menu.slider1.run();
+        synth_menu.slider2.run();
+        synth_menu.slider3.run();
+        synth_menu.slider4.run();
+    }
+    
+    if(mech_menu.pop_menu){
+        ofSetColor(100,100,100);
+        ofDrawRectangle(mech_menu.menu_x-5, mech_menu.menu_y-5, mech_menu.menu_width+10, mech_menu.menu_height+10);
+        ofSetColor(150,150,150);
+        ofDrawRectangle(mech_menu.menu_x, mech_menu.menu_y, mech_menu.menu_width, mech_menu.menu_height);
+        ofSetColor(150,0,255);
+        ofDrawRectangle(synth_menu.menu_x, synth_menu.menu_y, synth_menu.menu_width, 5);
+        
+        ofSetColor(0,0,255);
+        // in draw:
+        ofDrawBitmapString("MECHANICS MENU", mech_menu.menu_x+mech_menu.menu_width/2.0-100,mech_menu.menu_y+mech_menu.menu_height/6.0);
+        
+        mech_menu.button1.run();
+        mech_menu.button2.run();
+        mech_menu.button3.run();
+        mech_menu.slider1.run();
+        mech_menu.slider2.run();
+        mech_menu.slider3.run();
+    }
 }
 
 //--------------------------------------------------------------
-void ofApp::move(){
+void ofApp::random_v(){
     for(int i = 0; i<balls.size(); i++){
-        balls[i].set_v_x(2000);
+        balls[i].set_v_x(ofRandom(-1000,1000));
+        balls[i].set_v_y(ofRandom(-3000,0));
     }
 }
 
@@ -203,13 +372,27 @@ void ofApp::keyPressed(int key){
             OF_EXIT_APP(0);
             break;
         case ' ':
-            move();
+            random_v();
             break;
         case 'o':
             open_floor();
             break;
         case 'b':
             newBall(ofGetMouseX(),ofGetMouseY());
+            break;
+        case 'g':
+            for(int i =0; i<balls.size(); i++){
+                (balls[i].getmouse_g() == false) ? balls[i].setmouse_g(true) : balls[i].setmouse_g(false);
+            }
+            break;
+        case 'm':
+            (synth_menu.pop_menu == false) ? synth_menu.pop_menu = true : synth_menu.pop_menu = false;
+            break;
+        case 'n':
+            (mech_menu.pop_menu == false) ? mech_menu.pop_menu = true : mech_menu.pop_menu = false;
+            break;
+        case 'p':
+            (pause == false) ? pause = true : pause = false;
             break;
 //        case 'c':
 //            phase[2]=0;
@@ -242,16 +425,32 @@ void ofApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    if(synth_menu.pop_menu and (x>synth_menu.menu_x and x<synth_menu.menu_x+synth_menu.menu_width and y>synth_menu.menu_y and y<synth_menu.menu_y+10 or synth_menu.dragged)){
+        synth_menu.menu_x = ofGetMouseX()-synth_menu.drag_distx;
+        synth_menu.menu_y = ofGetMouseY()-synth_menu.drag_disty;
+    } else if(mech_menu.pop_menu and (x>mech_menu.menu_x and x<mech_menu.menu_x+mech_menu.menu_width and y>mech_menu.menu_y and y<mech_menu.menu_y+10 or mech_menu.dragged)){
+        mech_menu.menu_x = ofGetMouseX()-mech_menu.drag_distx;
+        mech_menu.menu_y = ofGetMouseY()-mech_menu.drag_disty;
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
+    if(synth_menu.pop_menu and x>synth_menu.menu_x and x<synth_menu.menu_x+synth_menu.menu_width and y>synth_menu.menu_y and y<synth_menu.menu_y+10){
+        synth_menu.drag_distx = ofGetMouseX()-synth_menu.menu_x;
+        synth_menu.drag_disty = ofGetMouseY()-synth_menu.menu_y;
+        synth_menu.dragged = true;
+    } else if(mech_menu.pop_menu and x>mech_menu.menu_x and x<mech_menu.menu_x+mech_menu.menu_width and y>mech_menu.menu_y and y<mech_menu.menu_y+10){
+        mech_menu.drag_distx = ofGetMouseX()-mech_menu.menu_x;
+        mech_menu.drag_disty = ofGetMouseY()-mech_menu.menu_y;
+        mech_menu.dragged = true;
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    synth_menu.dragged = false;
+    mech_menu.dragged = false;
 }
 
 //--------------------------------------------------------------
--- sound_demo/src/ofApp.h
+++ sound_demo/src/ofApp.h
@@ -9,6 +9,44 @@
 
 double distance(double x1, double y1, double x2, double y2);
 
+class toggle : public ofBaseApp{
+    private:
+        int  posx;
+        int  posy;
+        bool status = false; //On (True) - Off (False)
+        int  size = 20;
+        bool mouse_previous_pressed;
+    public:
+        void setup(int, int);
+        void run();
+    
+        int get_posx();
+        int get_posy();
+        bool get_status();
+    
+        void toggle_status();
+};
+
+class slider : public ofBaseApp{
+    private:
+        int  posx;
+        int  posy;
+        int ticker_y;
+        int  height = 100;
+        int  width  = 20;
+        int  value; // 0-100
+        bool selected; // Para continuar selecionado quando saimos das fronteiras do slider
+    public:
+        void setup(int, int);
+        void run();
+
+        int get_posx();
+        int get_posy();
+        int get_value();
+    
+        bool is_selected();
+};
+
 class Ball : public ofBaseApp{
         double posx;
         double posy;
@@ -17,12 +55,13 @@ class Ball : public ofBaseApp{
         double g = 5000;
         double a = 1000;
         double v_y = 0;
-         double v_x = 0;
+        double v_x = 0;
         int SAMPLE_RATE = 100;
         double dt = 1.0/SAMPLE_RATE;
         bool closed_floor = true;
         int note=1;
-    bool caught = false;
+        bool caught = false;
+        bool mouse_g = false;
     public:
         void set_posx(double);
         void move_x(double);
@@ -41,10 +80,37 @@ class Ball : public ofBaseApp{
         void set_closed_floor(bool val) {closed_floor = val;};
         int getNote();
         bool hit_note = false;
+        void setmouse_g(bool act) {mouse_g = act;};
+        bool getmouse_g() {return mouse_g;};
+};
+
+class menu : public ofBaseApp{
+    public:
+        menu() {};
+    
+        toggle button1;
+        toggle button2;
+        toggle button3;
+
+        slider slider1;
+        slider slider2;
+        slider slider3;
+    
+        slider slider4;
+    
+        int menu_x = 25;
+        int menu_y = 25;
+        int menu_width = 270;
+        int menu_height = 130;
+
+        bool pop_menu = false;
+        bool dragged = false;
+
+        int drag_distx = 0;
+        int drag_disty = 0;
 };
 
 class ofApp : public ofBaseApp{
-
     public:
         void setup();
         void update();
@@ -63,9 +129,10 @@ class ofApp : public ofBaseApp{
         void gotMessage(ofMessage msg);
     //balls
         void newBall(int, int);
-        void move();
+        void random_v();
         vector <Ball> balls;
         void open_floor();
+        bool pause;
     //audio
         void audioOut(ofSoundBuffer& );
         double envelope(double);
@@ -73,5 +140,8 @@ class ofApp : public ofBaseApp{
         double attack;
         double decay;
         int N;
-        
+    //MENUS
+        menu synth_menu;
+        menu mech_menu;
 };
+
