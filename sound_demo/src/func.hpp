dffce3bd76d88d19aa18a32986d82628ad6b47a4
Diogo Miguez <diogomiguez@MacBook-Pro-2.Home>
2021-06-27 17:11:42 +0100

    agora com os ficheiros

---
+++ sound_demo/src/func.hpp
@@ -0,0 +1,15 @@
+//
+//  func.hpp
+//  sound_demo
+//
+//  Created by Diogo Miguez on 27/06/2021.
+//
+
+#ifndef func_hpp
+#define func_hpp
+
+#include <stdio.h>
+
+double distance(double x1, double y1, double x2, double y2);
+
+#endif /* func_hpp */
